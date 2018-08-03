749006cf34d9dec3882ba6083ad2e25d44f798b8
Nguyen Quang Thai <thai@localhost.localdomain>
2018-08-04 00:47:40 +0700

    add post url

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const router = require('router');
 const finalhandler = require('finalhandler');
 const view = require('consolidate');
 const path = require('path');
+const level = require('level');
+const shortId = require('shortid');
+
+
+const pathDb = path.resolve('./db');
+const db = level(pathDb);
 
 const server = http.createServer();
 const app = new router();
 
 app.use((req, res, next) => {
-    res.render = function render(filename, params){
+    res.render = function render(filename, params) {
         let link = path.resolve(__dirname + '/view', filename);
-        view.mustache(link, params || {}, function(err, html) {
+        view.mustache(link, params || {}, function (err, html) {
             if (err) return next(err);
             res.setHeader('Content-Type', 'text/html');
             res.end(html);
@@ -19,14 +25,44 @@ app.use((req, res, next) => {
 
     next();
 })
+
+app.use((req, res, next) => {
+    res.json = function json(obj) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(obj));
+    }
+
+    next();
+})
+
 app.get('/', (req, res) => {
     res.render('home.html', {
-        name : 'thai'
+        name: 'thai'
+    })
+})
+
+app.post('/', (req, res) => {
+    if (!req.body.url) {
+        res.render('home.html', {
+            msg: "URL is required"
+        })
+    }
+
+    let id = shortId.generate();
+    db.put(id, req.body.url, (err) => {
+        if (err) return res.render('home.html', {
+            msg: err.toString();
+        })
+
+        let url = 'thaidzvl.com/' + id;
+        res.render('home.html', {
+            msg: `URL: ${url}`
+        })
     })
 })
 
 
-server.on('request', (req,res) => {
+server.on('request', (req, res) => {
     app(req, res, finalhandler)
 })
 server.listen(process.env.PORT || 7777);
